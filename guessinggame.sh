ef92dad80056aa23da8adf2f81fcbae6ee7b33c3
Kay Jan WONG <kay.jan@hotmail.com>
2021-06-30 18:34:06 +0800

    update

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,22 +1,22 @@
-function guessinggame {
-  echo "Guess the number of files in this directory"
+function guessinggame() {
   ans=$(ls | wc -l)
-  read input
-
-  if [[ $input > $ans ]]
-  then
-    echo "Wrong! Try a lower number"
-  elif [[ $input < $ans ]]
-  then
-    echo "Wrong! Try a larger number"
-  else
-    echo "Correct! Congrats!"
-    let correct=true
-  fi
+  correct=false
+ 
+  while [ $correct == false ]
+  do
+	echo "Guess the number of files in this directory"
+	read input
+	if [ $input -gt $ans ]
+	then
+		echo "Wrong! Try a lower number"
+	elif [ $input -lt $ans ]
+	then
+		echo "Wrong! Try a larger number"
+	else
+		echo "Correct! Congrats!"
+		let correct=true
+	fi
+  done
 }
 
-correct=false
-while [[ $correct == false ]]
-do
-  guessinggame
-done
+guessinggame
